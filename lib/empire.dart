836ec61dff3cc391b4d9e7a8bfc0e0927b2097de
jshep23 <justin.shepertycki@gmail.com>
2022-06-07 08:44:31 -0500

    initial commit

--- lib/empire.dart
+++ lib/empire.dart
@@ -1,7 +1,101 @@
 library empire;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+abstract class EmpireWidget<T extends EmpireViewModel> extends StatefulWidget {
+  final T viewModel;
+
+  const EmpireWidget({
+    Key? key,
+    required this.viewModel,
+  }) : super(key: key);
+
+  EmpireState<EmpireWidget, T> createEmpire();
+
+  @override
+  State<StatefulWidget> createState() {
+    // ignore: no_logic_in_create_state
+    return createEmpire();
+  }
+}
+
+abstract class EmpireState<T extends EmpireWidget, E extends EmpireViewModel> extends State<T> {
+  final E viewModel;
+
+  EmpireState(this.viewModel) {
+    viewModel.listen((_) {
+      setState(() {});
+    });
+  }
+
+  Widget render(BuildContext context);
+
+  @override
+  @mustCallSuper
+  void dispose() {
+    viewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return render(context);
+  }
+}
+
+abstract class EmpireViewModel {
+  final StreamController<EmpireStateChanged> _stateController = StreamController.broadcast();
+  late final Stream<EmpireStateChanged> _stream;
+  final List<StreamSubscription> _subscriptions = [];
+
+  EmpireViewModel() {
+    _stream = _stateController.stream;
+    initProperties();
+  }
+
+  void initProperties();
+
+  void listen(Function(EmpireStateChanged) onStateChanged) {
+    _subscriptions.add(_stream.listen(onStateChanged));
+  }
+
+  void notify(EmpireStateChanged event) {
+    _stateController.add(event);
+  }
+
+  void dispose() {
+    for (var sub in _subscriptions) {
+      sub.cancel();
+    }
+    _stateController.close();
+  }
+}
+
+abstract class EmpireValue<T> {
+  T? get value;
+}
+
+class EmpireProperty<T> implements EmpireValue<T> {
+  T _value;
+  @override
+  T get value => _value;
+
+  final EmpireViewModel _viewModel;
+
+  EmpireProperty(this._value, this._viewModel);
+
+  void call(T value) {
+    final previousValue = _value;
+    _value = value;
+    _viewModel.notify(EmpireStateChanged(value, previousValue));
+  }
+}
+
+class EmpireStateChanged<T> {
+  final T previousValue;
+  final T nextValue;
+
+  EmpireStateChanged(this.nextValue, this.previousValue);
 }
--- test/empire_test.dart
+++ test/empire_test.dart
@@ -1,12 +1,57 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:empire/empire.dart';
 
+class TestViewModel extends EmpireViewModel {
+  late EmpireProperty<String?> name;
+
+  @override
+  void initProperties() {
+    name = EmpireProperty(null, this);
+  }
+}
+
+class MyWidget extends EmpireWidget<TestViewModel> {
+  const MyWidget({
+    Key? key,
+    required TestViewModel viewModel,
+  }) : super(key: key, viewModel: viewModel);
+
+  @override
+  EmpireState<EmpireWidget<EmpireViewModel>, TestViewModel> createEmpire() {
+    return _MyWidgetState(viewModel);
+  }
+}
+
+class _MyWidgetState extends EmpireState<MyWidget, TestViewModel> {
+  _MyWidgetState(super.viewModel);
+
+  @override
+  Widget render(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text(viewModel.name.value ?? '')),
+      ),
+    );
+  }
+}
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('Test', (tester) async {
+    final viewModel = TestViewModel();
+    viewModel.name("Justin");
+    await tester.pumpWidget(MyWidget(
+      viewModel: viewModel,
+    ));
+
+    final text = find.text("Justin");
+    expect(text, findsOneWidget);
+
+    viewModel.name("Shep");
+    await tester.pumpAndSettle();
+
+    final textTwo = find.text("Shep");
+    expect(textTwo, findsOneWidget);
   });
 }
