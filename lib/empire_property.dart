c2e20c1ce48536f89a0774c396d4c90d329706c0
jshep23 <justin.shepertycki@gmail.com>
2022-06-13 22:18:56 -0500

    added int and nullable int properties

--- lib/empire_property.dart
+++ lib/empire_property.dart
@@ -765,3 +765,51 @@ class EmpireNullableStringProperty extends NullableEmpireProperty<String?> {
   /// [end], if provided, must be greater than or equal to [start].
   String? substring(int start, [int? end]) => _value?.substring(start, end);
 }
+
+class EmpireIntProperty extends EmpireProperty<int> {
+  EmpireIntProperty(super.value, super.viewModel);
+
+  /// Returns true if the int value is odd
+  bool get isOdd => _value.isOdd;
+
+  /// Returns true if the int value is even.
+  bool get isEven => _value.isEven;
+
+  /// Whether this number is negative.
+  bool get isNegative => _value.isNegative;
+
+  /// The int value as a double
+  double toDouble() => _value.toDouble();
+
+  /// Returns the absolute value of this integer.
+  int abs() => _value.abs();
+}
+
+class EmpireNullableIntProperty extends NullableEmpireProperty<int?> {
+  EmpireNullableIntProperty(super.value, super.viewModel);
+
+  /// Returns true if the int value is odd
+  ///
+  /// Returns false if the int value is null
+  bool get isOdd => _value?.isOdd ?? false;
+
+  /// Returns true if the int value is even.
+  ///
+  /// Returns false if the int value is null
+  bool get isEven => _value?.isEven ?? false;
+
+  /// Whether this number is negative.
+  ///
+  /// Returns false if the int value is null
+  bool get isNegative => _value?.isNegative ?? false;
+
+  /// The int value as a double
+  ///
+  /// Returns null if the int value is null
+  double? toDouble() => _value?.toDouble();
+
+  /// Returns the absolute value of this integer.
+  ///
+  /// Returns null if the int value is null
+  int? abs() => _value?.abs();
+}
