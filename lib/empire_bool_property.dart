21edc2d1da7800257c960fc160150d721882afbb
Michael Dryden <mjdryden@gmail.com>
2022-06-16 11:52:06 -0500

    New helper set methods for bool properties

--- lib/empire_bool_property.dart
+++ lib/empire_bool_property.dart
@@ -15,6 +15,12 @@ class EmpireBoolProperty extends EmpireProperty<bool> {
 
   ///Whether the underlying value is false
   bool get isFalse => !_value;
+
+  ///Sets the value to true
+  void setTrue() => super.set(true);
+
+  ///Sets the value to false
+  void setFalse() => super.set(false);
 }
 
 ///An [EmpireProperty] with similar characteristics as a an
@@ -25,11 +31,21 @@ class EmpireBoolProperty extends EmpireProperty<bool> {
 ///When the value of this changes, it will send a [EmpireStateChanged] event by default. This includes
 ///automatically triggering a UI rebuild.
 class EmpireNullableBoolProperty extends EmpireProperty<bool?> {
-  EmpireNullableBoolProperty(super.value, super.viewModel, {super.propertyName});
+  EmpireNullableBoolProperty(super.value, super.viewModel,
+      {super.propertyName});
 
   ///Whether the underlying value is not null and true
   bool get isTrue => isNotNull && _value == true;
 
   ///Whether the underlying value is not null and false
   bool get isFalse => isNotNull && _value == false;
+
+  ///Sets the value to true
+  void setTrue() => super.set(true);
+
+  ///Sets the value to false
+  void setFalse() => super.set(false);
+
+  ///Sets the value to null
+  void setNull() => super.set(null);
 }
