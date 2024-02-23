1d0d05596db7327ab3a88efee59abe1154611326
jshep23 <justin.shepertycki@gmail.com>
2024-02-23 13:08:07 -0600

    Added link between view model and widget instance. Prevent shared view models

--- lib/src/empire_exceptions.dart
+++ lib/src/empire_exceptions.dart
@@ -1,10 +1,15 @@
 ///Base class for any Empire specific exception
 abstract class EmpireException implements Exception {
   final StackTrace? stack;
-  final String? propertyName;
   final Type type;
 
-  EmpireException(this.stack, this.propertyName, this.type);
+  EmpireException(this.stack, this.type);
+}
+
+abstract class EmpirePropertyException extends EmpireException {
+  final String? propertyName;
+
+  EmpirePropertyException(super.stack, this.propertyName, super.type);
 }
 
 ///Thrown when an not-null-safe action is performed on a Nullable Empire Property, and the underlying value
@@ -12,7 +17,7 @@ abstract class EmpireException implements Exception {
 ///
 ///An example is performing an arithmetic operation on an [EmpireNullableIntProperty] when the int value
 ///was null
-class EmpirePropertyNullValueException extends EmpireException {
+class EmpirePropertyNullValueException extends EmpirePropertyException {
   EmpirePropertyNullValueException(super.stack, super.propertyName, super.type);
 
   @override
@@ -23,11 +28,20 @@ class EmpirePropertyNullValueException extends EmpireException {
 ///Thrown when an EmpireProperty value is changed and attempts to update the UI, but has not been added to the
 ///empireProps property of the associated EmpireViewModel
 ///
-class PropertyNotAssignedToEmpireViewModelException extends EmpireException {
+class PropertyNotAssignedToEmpireViewModelException
+    extends EmpirePropertyException {
   PropertyNotAssignedToEmpireViewModelException(
       super.stack, super.propertyName, super.type);
 
   @override
   String toString() =>
       'PropertyNotAssignedToEmpireViewModelException: The value for ${propertyName ?? type} changed and failed to update the UI. Did you forget to add it to empireProps?.\nStack: $stack';
+}
+
+class EmpireViewModelAlreadyAssignedException extends EmpireException {
+  EmpireViewModelAlreadyAssignedException(super.stack, super.type);
+
+  @override
+  String toString() =>
+      'EmpireViewModelAlreadyAssignedException: The View Model of type $type has already been assigned to a different EmpireWidget.\nStack: $stack';
 }
